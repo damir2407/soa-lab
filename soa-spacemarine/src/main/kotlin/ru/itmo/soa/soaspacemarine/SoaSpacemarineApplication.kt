b54b1611fbb689f0232144e08e9d29513aa67f9b
damir2407 <dbaltabaev8708@gmai.com>
2024-02-23 20:38:59 +0300

    init

---
+++ soa-spacemarine/src/main/kotlin/ru/itmo/soa/soaspacemarine/SoaSpacemarineApplication.kt
@@ -0,0 +1,11 @@
+package ru.itmo.soa.soaspacemarine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SoaSpacemarineApplication
+
+fun main(args: Array<String>) {
+	runApplication<SoaSpacemarineApplication>(*args)
+}
---
+++ soa-starship/src/main/kotlin/ru/itmo/soa/soastarship/HelloApplication.kt
@@ -0,0 +1,9 @@
+package ru.itmo.soa.soastarship
+
+import jakarta.ws.rs.ApplicationPath
+import jakarta.ws.rs.core.Application
+
+@ApplicationPath("/api")
+class HelloApplication : Application() {
+
+}
