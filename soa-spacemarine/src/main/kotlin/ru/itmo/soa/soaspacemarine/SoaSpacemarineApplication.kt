980ef422bf078560522106005618df008a04c660
Nurullaev Daniil <d.nurullaev@tinkoff.ru>
2024-02-24 22:08:46 +0300

    fix haproxy

--- soa-spacemarine/src/main/kotlin/ru/itmo/soa/soaspacemarine/SoaSpacemarineApplication.kt
+++ soa-spacemarine/src/main/kotlin/ru/itmo/soa/soaspacemarine/SoaSpacemarineApplication.kt
@@ -2,9 +2,21 @@ package ru.itmo.soa.soaspacemarine
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
 
 @SpringBootApplication
-class SoaSpacemarineApplication
+@RestController
+class SoaSpacemarineApplication{
+	@GetMapping("/my-health-check")
+	fun myCustomCheck(): ResponseEntity<String> {
+		val message = "Testing my healh check function"
+		return ResponseEntity<String>(message, HttpStatus.OK)
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<SoaSpacemarineApplication>(*args)
